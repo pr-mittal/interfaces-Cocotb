4e2784b773c6aedcdbbfc08ec1b7fe25f9dd3f97
siliconmerc <2001pranavmittal@gmail.com>
2023-03-08 13:31:09 +0530

    Added directed test for len==0 or data ==0

--- tests/TC_2_LEN_DATA0.py
+++ tests/TC_2_LEN_DATA0.py
@@ -1,63 +1,78 @@
-# # Feature : Randomised LEN and SUM 0 operation
-# # Description : DUT Operation when Data length is 0 or Data is 0
-# # When we feed an invalid length 0 then the data.en pin should remain low i.e the design should not accept data . Although if length is non zero then the data sum of zero should also be allowed.
-# # Scenario : Randomised corner case test
-# # Given : Unit Test Environment
+# Feature : Randomised LEN and SUM 0 operation
+# Description : DUT Operation when Data length is 0 or Data is 0
+# When we feed an invalid length 0 then the data.en pin should remain low i.e the design should not accept data . Although if length is non zero then the data sum of zero should also be allowed.
+# Scenario : Randomised corner case test
+# Given : Unit Test Environment
 
-# import cocotb
-# from cocotb.clock import Clock
-# from cocotb.triggers import Timer, RisingEdge, ReadOnly,NextTimeStep,FallingEdge
-# import random
-# from bin.driver import InputDriver,OutputDriver
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge, ReadOnly,NextTimeStep,FallingEdge
+import random
+from bin.driver import InputDriver,OutputDriver,ConfigIODriver
 
-# def sb_fn(actual_value):
-# 	global expected_value
-# 	assert actual_value==expected_value.pop(0),f"Scoreboard(SB) Matching Failed"
-# def get_max_value(Nbits):
-# 	#signed bit representation
-# 	return  2**(Nbits - 1)-1
+def sb_fn(actual_value):
+	global expected_value
+	assert actual_value==expected_value.pop(0),f"Scoreboard(SB) Matching Failed"
+def get_max_value(Nbits):
+	#signed bit representation
+	return  2**(Nbits - 1)-1
+def cfg_sb_fn(actual_value):
+	global cfg_expected_value
+	assert actual_value==expected_value.pop(0),f"Scoreboard(SB) Matching Failed"
 
-# @cocotb.test()
-# async def dut_test(dut):
-# 	cocotb.start_soon(Clock(dut.CLK, 5,'ns').start())
-# 	global expected_value
-# 	regressions=1
-			
-# 	random.randint(3, 9)
-# 	# a=(0,0,1,1)
-# 	# l=(0,1,0,1)
-# 	#y=(0,1,1,0)
-# 	#expected_value=(0,1,1,0)
-# 	expected_value=[]
-# 	dut.RST_N.value=1
-# 	await Timer(1,'ns')
-# 	dut.RST_N.value=0
-# 	await Timer(1,'ns')
-# 	await RisingEdge(dut.CLK)
-# 	dut.RST_N.value=1
+@cocotb.test()
+async def dut_test(dut):
+	cocotb.start_soon(Clock(dut.CLK, 5,'ns').start())
+	global expected_value
+	regressions=5
 	
-# 	dindrv=InputDriver(dut,'din',dut.CLK)
-# 	ldrv=InputDriver(dut,'len',dut.CLK)
-# 	OutputDriver(dut,'dout',dut.CLK,sb_fn)
+	expected_value=[]
+	dut.RST_N.value=1
+	await Timer(1,'ns')
+	dut.RST_N.value=0
+	await Timer(1,'ns')
+	await RisingEdge(dut.CLK)
+	dut.RST_N.value=1
 	
-# 	for i in range(regressions):
-# 		l=random.randint(0,get_max_value(8))		
-# 		for l in [0,5]:
-# 			a=[]
-# 			sum=0
-# 			if l!=0: 
-# 				for j in range(l):
-# 					val=0
-# 					sum+=val
-# 					a.append(val)
-# 				expected_value.append(sum)
+	dindrv=InputDriver(dut,'din',dut.CLK)
+	ldrv=InputDriver(dut,'len',dut.CLK)
+	OutputDriver(dut,'dout',dut.CLK,sb_fn)
+	pause_mode=True #have to feed the value of length always
+	cfgdrv=ConfigIODriver(dut,'cfg',dut.CLK,cfg_sb_fn)
 
-# 			for j in range(2):	
-# 				if(j==0):# length
-# 					for k in range(len(a)):
-# 						dindrv.append(a[k])
-# 					ldrv.append(l)
-# 				# if(j==1): # register map
-# 	#wait for all calculations to complete
-# 	while len(expected_value)>0:
-# 		await Timer(2,'ns')
+	if(pause_mode):
+		cfgdrv.append([1,4,2])
+	else:
+		l=random.randint(0,10)
+		# l=5
+		ldrv.append(l)
+	
+	for i in range(regressions):
+		if(pause_mode):
+			k=random.randint(0,1)
+			if(k):
+				l=0 #drive zero
+			else:
+				l=random.randint(0,10)
+			# l=5
+			ldrv.append(l)
+		if(l==0):continue
+		a=[]
+		sum=0
+		for j in range(l):
+			# val=random.randint(-get_max_value(8)-1,get_max_value(8))
+			# val=random.randint(0,32)
+			val=0 #all data values are 0
+			sum+=val
+			a.append(val)
+		print(f'SUM={sum}')
+		expected_value.append(sum)
+		
+		for j in range(2):	
+			if(j==0):# length
+				for k in range(len(a)):
+					dindrv.append(a[k])
+			# if(j==1): # register map
+	#wait for all calculations to complete
+	while len(expected_value)>0:
+		await Timer(2,'ns')
