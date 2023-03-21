9995f6359f43c9fe948226bafa48de7b982f8271
siliconmerc <2001pranavmittal@gmail.com>
2023-03-21 12:58:38 +0530

    Added basic protocol verif

--- tests/TC_5_PROTOCOL_VERIF.py
+++ tests/TC_5_PROTOCOL_VERIF.py
@@ -12,39 +12,185 @@
 # Previous state = [Idle,Ready,Transaction]
 # Cross : Current state x Previous state
 
-# from cocotb_coverage.coverage import CoverCross,CoverPoint
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge, ReadOnly,NextTimeStep,FallingEdge
+import random
+from bin.driver import InputDriver,OutputDriver,ConfigIODriver,dutDriver
+from bin.sequencer import PacketGenerator,dutSequencer
+from bin.scoreboard import ScoreBoard
+from bin.monitor import IOMonitor
+import os
+from cocotb_coverage.coverage import CoverCross,CoverPoint,coverage_db
+# class en_rdy:
+# 	def __init__(self,name):
+# 		self.name=name
+# 	def __call__(self):
+# 		self.a_prot_cover()
+# 	@CoverPoint("top.prot."+self.name+".current",
+# 			xf=lambda x:x['current'],
+# 			bins=['Idle','Rdy','Txn']
+# 			)
+# 	@CoverPoint("top.prot.a.previous",
+# 			xf=lambda x:x['previous'],
+# 			bins=['Idle','Rdy','Txn']
+# 			)
+# 	@CoverCross("top.cross.a_prot.cross",
+# 			items=["top.prot.a.previous","top.prot.a.current"]
+# 			,ign_bins=[('Rdy','Idle')]
+# 			)
+# 	def a_prot_cover(txn):
+# 		pass
+@CoverPoint("top.prot.len.current",
+		xf=lambda x:x['current'],
+		bins=['Idle','Rdy','Txn']
+		)
+@CoverPoint("top.prot.len.previous",
+		xf=lambda x:x['previous'],
+		bins=['Idle','Rdy','Txn']
+		)
+@CoverCross("top.cross.len_prot.cross",
+		items=["top.prot.len.previous","top.prot.len.current"]
+		,ign_bins=[('Rdy','Idle')]
+		)
+def len_prot_cover(txn):
+	pass
 
-# @CoverPoint("top.a",# noga F405
-# 	    xf=lambda x,y:x,
-# 		bins=[0,1])
-# @CoverPoint("top.b",# noga F405
-# 	    xf=lambda x,y:x,
-# 		bins=[0,1])
-# @CoverCross("top.cross.ab",
-# 	    items=["top.a","top.b"
-# 	    ])
-# def ab_cover(a,b):
-# 	pass
+@CoverPoint("top.prot.din.current",
+		xf=lambda x:x['current'],
+		bins=['Idle','Rdy','Txn']
+		)
+@CoverPoint("top.prot.din.previous",
+		xf=lambda x:x['previous'],
+		bins=['Idle','Rdy','Txn']
+		)
+@CoverCross("top.cross.din_prot.cross",
+		items=["top.prot.din.previous","top.prot.din.current"]
+		,ign_bins=[('Rdy','Idle')]
+		)
+def din_prot_cover(txn):
+	pass
 
-# #in function
-# # ab_cover(a,b) 
+@CoverPoint("top.prot.dout.current",
+		xf=lambda x:x['current'],
+		bins=['Idle','Rdy','Txn']
+		)
+@CoverPoint("top.prot.dout.previous",
+		xf=lambda x:x['previous'],
+		bins=['Idle','Rdy','Txn']
+		)
+@CoverCross("top.cross.dout_prot.cross",
+		items=["top.prot.dout.previous","top.prot.dout.current"]
+		,ign_bins=[('Rdy','Idle')]
+		)
+def dout_prot_cover(txn):
+	pass
+
+@cocotb.test()
+async def dut_test(dut):
+	cocotb.start_soon(Clock(dut.CLK, 5,'ns').start())
+	global expected_value
+	regressions=5
+	
+	expected_value=[]
+	dut.RST_N.value=1
+	await Timer(1,'ns')
+	dut.RST_N.value=0
+	await Timer(1,'ns')
+	await RisingEdge(dut.CLK)
+	dut.RST_N.value=1
+	
+	outSB=ScoreBoard('dout')
+	cfgSB=ScoreBoard('cfg')
+	drv=dutDriver({'cfg':cfgSB,'dout':outSB})
+	dindrv=InputDriver(dut,'din',dut.CLK,drv)
+	ldrv=InputDriver(dut,'len',dut.CLK,drv)
+	outDrv=OutputDriver(dut,'dout',dut.CLK,drv,outSB)
+	cfgdrv=ConfigIODriver(dut,'cfg',dut.CLK,drv,cfgSB)
+	IOMonitor(dut,'len',dut.CLK,callback=len_prot_cover)
+	IOMonitor(dut,'din',dut.CLK,callback=din_prot_cover)
+	IOMonitor(dut,'dout',dut.CLK,callback=dout_prot_cover)
+
+	#in function
+	
+	#in input and output driver
+	# for i in range(random.randint(0,20)):
+	# 	await RisingEdge(self.clk)
 
 
-# @CoverPoint("top.prot.a.current",# noga F405
-# 	    xf=lambda x:x['current'],
-# 		bins=['Idle','Rdy','Txn']
-# 		)
-# @CoverPoint("top.prot.a.previous",# noga F405
-# 	    xf=lambda x:x['previous'],
-# 		bins=['Idle','Rdy','Txn']
-# 		)
-# @CoverCross("top.cross.a_prot.cross",
-# 	    items=["top.prot.a.previous","top.prot.a.current"]
-# 	    ,ign_bins=[('Rdy','Idle')]
-# 	    )
-# def a_prot_cover(txn):
-# 	pass
+	# pause_mode=True #have to feed the value of length always
+	# sw_override=True
 
-# # coverage_db.report_coverage(cocotb.log.info,bins=True)
-# # coverage_file=os.path.join(os.getenv("RESULT_PATH","./"),'coverage.xml')
-# # coverage_db.export_to_xml(filename=coverage_file)
+	seq=dutSequencer()
+	gen=PacketGenerator()
+	packet=gen.get()
+	seq.cfg_address_4(cfgdrv,packet)
+
+	for i in range(regressions):
+		# if(pause_mode):
+		# k=random.randint(0,1)
+		# if(k):
+		# 	# l=0 #drive zero
+		# 	packet['len_value']=0
+		# 	packet['din_value']=gen.array_w_sum(packet['din_sum'],0)
+		# 	else:
+		# 		l=random.randint(0,10)
+		# 	# l=5
+		# 	ldrv.append(l)
+		l=seq.length_sequencer(cfgdrv,ldrv,packet,False)
+		# if(l==0):continue
+		if(l!=packet['len_value']):
+			packet['din_value']=gen.array_w_sum(packet['din_sum'],l)
+		for val in packet['din_value']:
+			dindrv.append(val)
+		if l!=0:
+			print(f'Calculated SUM={packet["din_sum"]} length {l}')
+			drv._send('dout',packet['din_sum'])
+		# k=random.randint(0,1)
+		# if(k):
+		# 	l=seq.length_sequencer(cfgdrv,ldrv,0,False,pause_mode,sw_override)
+		# else:
+		# 	l=seq.length_sequencer(cfgdrv,ldrv,0,True,pause_mode,sw_override)
+		# if(l==0):continue
+		# a=[]
+		# sum=0
+		# for j in range(l):
+		# 	# val=random.randint(-get_max_value(8)-1,get_max_value(8))
+		# 	# val=random.randint(0,32)
+		# 	val=0 #all data values are 0
+		# 	sum+=val
+		# 	a.append(val)
+		# print(f'Calculated SUM={sum}')
+		# expected_value.append(sum)
+		
+		# for j in range(2):	
+		# 	if(j==0):# length
+		# 		for k in range(len(a)):
+		# 			dindrv.append(a[k])
+		# 	# if(j==1): # register map
+		packet=gen.get()
+		seq.cfg_address_4(cfgdrv,packet)
+		# if(packet['cfg_op']):
+		print(packet)
+
+		while(len(dindrv._sendQ)!=0 or len(ldrv._sendQ)!=0  or len(cfgdrv._sendQ)!=0 or dut.len_en!=0 or dut.din_en!=0 or dut.cfg_en!=0):
+		# while(drv.busy):
+			# await Timer(2,'ns')
+			if random.randint(0,1) and len(cfgdrv._sendQ)==0 and dut.cfg_en==0 and drv.current_count<drv.programmed_length-1 : 
+				r_cfg_pkt=gen.get_cfg_r()
+				# print(r_cfg_pkt)
+				seq.cfg_r(cfgdrv,r_cfg_pkt)
+				# pass
+			await RisingEdge(dut.CLK)
+			await ReadOnly()
+	#wait for all calculations to complete
+	# while len(expected_value)>0:
+	# 	await Timer(2,'ns')
+	print("WAITING FOR OUTPUT")
+	while (not (cfgSB.is_empty() and outSB.is_empty())):
+		await RisingEdge(dut.CLK)
+		# await Timer(2,'ns')
+
+	coverage_db.report_coverage(cocotb.log.info,bins=True)
+	coverage_file=os.path.join(os.getenv("RESULT_PATH","./"),'datapath_coverage.xml')
+	coverage_db.export_to_xml(filename=coverage_file)
--- tests/bin/monitor.py
+++ tests/bin/monitor.py
@@ -2,18 +2,9 @@ from cocotb.triggers import Timer, RisingEdge, ReadOnly,NextTimeStep,FallingEdge
 from cocotb_bus.monitors import BusMonitor
 
 # IOMonitor(dut,'a',dut.clk,callback=a_prot_cover)
-
 class IOMonitor(BusMonitor):
-	_signals=['rdy','en','data']
-	async def monitor_recv(self):
-		#Generally signal are driven on rising edge and will be stable by falling edge, So we sample everything at falling edge of the clock
-		fallingedge=FallingEdge(self.clk)
-		rdonly=ReadOnly()
-		#Wait for the trigger signal to be asserted. Could be either Request or Grant depending on the protocol
-		# We are doing this because the signals are not properly driven at reset and sometimes even post reset. Waiting for some control signal to have a value is an proxy indicator that the device has come out of the reset stage.
-		# re.
-		#await Rising Edge(self.bus.req)
-		#NB could await on valid here more efficiently?
+	_signals = ['rdy','en','value']
+	async def _monitor_recv(self):
 		phases={
 			0:'Idle',
 			1: 'RDY',
@@ -21,19 +12,10 @@ class IOMonitor(BusMonitor):
 		}
 		prev='Idle'
 		while True:
-			await fallingedge
-			await rdonly
-			if self.bus.req.value.integer == 1 and self.bus.ready.value== 1: # Maybe binstr instead of integer will be better he
-			# TODO Capture transaction in a variable called TXN
-				#burst = self.bus.burst_size.value.integer
-				#row self.bus.addr_row.value.integer
-				#col= self.bus.addr_col.value.integer
-				#slave_id = self.bus.addr_slave_id.value. integer
-				# ram_address = self.bus.addr_ram_address.value.integer
-				# txn = {'row': row, 'col': col, slave_id': slave_id, 'ram_address': ram_address, 'burst': burst}
-				#self.log.info("[MReq Monitor Rx] Recieved Transaction" + repr(txn))
-			# TODO: Optional:- Before recording on the scoreboard we may have to transform the data in a format that the Sibling Monior uses.
+			await FallingEdge(self.clock)
+			await ReadOnly()
+			txn=(self.bus.en.value<<1)|self.bus.rdy.value
+			self._recv({'previous':prev,'current':phases[txn]})
+			prev=phases[txn]
 			#Record the transaction in the scoreboard.
-				txn=(self.bus.en.value<<1)|self.bus.rdy.value
-				self._recv({'previous':prev,'current':phases[txn]})
-				prev=phases[txn]
+			# self.log.info("[MReq Monitor Rx] Recieved Transaction" + repr(txn))
